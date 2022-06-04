5f05525c4e5ee4407313d4c185da0b07281877af
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-06-04 10:04:43 +0200

    add dynamic route

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,12 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/hello/<name>")]
+fn hello(name: &str) -> String {
+    format!("Hi {}, nice to meet you!", name)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, hello])
 }
